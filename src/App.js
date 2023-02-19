32e83271be589a370be1022cdb71bc8ccd917569
Abdel Droid <106479409+abdessattar23@users.noreply.github.com>
2023-02-19 14:28:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route }
+	from "react-router-dom";
+import SignIn from "./Screens/Signin";
+import SignUp from "./Screens/Signup";
+import ChatHome from "./Screens/ChatHome";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+	<div className="App">
+	<BrowserRouter>
+		<Routes>
+		<Route exact path="/"
+			element={<SignIn />} />
+		<Route path="/Signup"
+			element={<SignUp />} />
+		<Route path="/chat-home/:receiverId"
+			element={<ChatHome />} />
+		</Routes>
+	</BrowserRouter>
+	</div>
+);
 }
 
 export default App;
+
